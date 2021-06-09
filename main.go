822f2c6a2c09a3441819e57816651f8fb323beb9
Gyro7 <gyro@sach1.tk>
2021-06-09 11:49:00 +0200

    Tidied the TODO list once again.

--- main.go
+++ main.go
@@ -8,4 +8,5 @@ func main() {
 }
 
 //TODO add double check to see if chapters were skipped (e.g. chapter 37 exists, chapter 38 doesn't but the rest of the series does)
-//TODO change host to https://www.mangahere.cc/
+//TODO add option to delete a downloaded manga
+//TODO (long future) change host to https://www.mangahere.cc/
