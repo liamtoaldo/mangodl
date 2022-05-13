92b0b05bc63fe260351852c2a81ef13b27c7c946
4ce <88108711+4cecoder@users.noreply.github.com>
2022-05-13 02:31:26 -0400

    Update install.sh
    https://unix.stackexchange.com/questions/8656/usr-bin-vs-usr-local-bin-on-linux

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 ## UNCOMMENT THESE LINES IF YOU WANT TO BUILD FROM SOURCE
 #echo Removing pre-built binary
 #rm mangodl
+#rm /usr/local/bin/mangodl
 #echo Installing the needed dependencies
 #go get -d ./...
 #echo Building...
-#go build
+#go build main.go
+#mv main mangodl
 
 # make the file executable
 chmod +x mangodl
 # install the executable to /usr/bin
-sudo install -m755 mangodl /usr/bin/mangodl
+sudo install -m755 mangodl /usr/local/bin/mangodl
